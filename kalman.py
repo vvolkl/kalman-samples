ef9a9a09294f8a7387708116a9fac6f49454a5fc
vvolkl <valentin.voelkl@student.uibk.ac.at>
2015-11-24 15:39:49 +0100

    fix typo, switch to np.array instead of np.matrix, fix some dimensionality problem

--- kalman.py
+++ kalman.py
@@ -6,24 +6,27 @@ import matplotlib.pyplot as plt
 
 def kalman_predict( A, # transition matrix 
             r, # measurement error matrix
-            H, # transformation matrix from state vector to measurement
+            H_k, # list of transformation matrices from state vector to measurement
             p, # initial variance on prediction
             xkal, # estimated state vector
             xpredict, # predicted state vector
             xmeas): # measurements
     for i in range(1, xkal.shape[1]): # for each measurement do 
+        # shorthand for current transformation matrix
+        H = H_k[i]
         # prediction: recursive formula
         xpredict[:, i] = np.dot(A, xkal[:, i - 1])
         # predict covariance  
-        p = A*p*A.T
+        p = np.dot(np.dot(A, p), A.T)
         # construct kalman gain matrix according to prediction equations
         # higher gain leads to higher influence of measurement,
         # lower gain to higher influence of predicion
-        K = np.dot(p*H.T, np.linalg.inv(H*p*H.T + r))
+        K = np.dot(np.dot(p, H.T), 
+            np.linalg.inv(np.dot(np.dot(H, p), H.T) + r))
         # construct estimate from prediction and gain 
-        xkal[:, i] = xpredict[:, i] + K*(xmeas[:, i] - H*xpredict[:, i])
+        xkal[:, i] = xpredict[:, i] + np.dot(K, (xmeas[:, i] - np.dot(H, xpredict[:, i])))
         # update covariance with gain
-        p = (np.identity(K.shape[0]) - K * H) * p
+        p = np.dot(np.eye(K.shape[0]) - np.dot(K, H), p)
     return xkal, xpredict
 
 def plot_results(xkal, xpredict, xmeas, xtrue):
